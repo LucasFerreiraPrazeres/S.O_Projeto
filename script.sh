366289e9229842971228e71f4d3f413e81914b02
LucasFerreiraPrazeres <luucasfp0@gmail.com>
2021-05-25 10:21:58 -0300

    Alteração no Script

--- script.sh
+++ script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+
+
 TIMEFORMAT=%R
 total=0
 
-for i in i$(seq 1 500); do
-	#time=$({ time python3 fernet.py &>/dev/null; } 2>&1)
+for i in $(seq 1 500); do
+	
 	tempo=$({ //usr/bin/time --format='tempo=%E
 memoria= %M kernel=%S usr=%U' python3 fernet.py &>/dev/null; } 2>&1)
 	total=$(echo "scale=4;$total + $tempo" | bc)
